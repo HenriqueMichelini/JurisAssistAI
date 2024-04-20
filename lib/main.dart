5bf14f6b6147d380cb5fbac77a53b021eabd8888
Henrique Michelini <henrique.g.michelini@gmail.com>
2024-04-19 23:24:54 -0300

    just saving for cloning on another pc later

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:jaai/pages/dashboard.dart';
 import 'package:jaai/pages/register.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(149, 124, 18, 6),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        scaffoldBackgroundColor: kColorScheme.primary,
+      ),
       home: Register(),
     ),
   );
